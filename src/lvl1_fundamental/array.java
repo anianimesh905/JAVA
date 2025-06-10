aeb240ecfd74d6cccdd09809fdebbdd6ad99a4e9
Animesh Singh <anianimesh905@gmail.com>
2025-06-10 16:36:52 +0530

    Remove IDE files from tracking

---
+++ src/lvl1_fundamental/array.java
@@ -0,0 +1,9 @@
+package src.lvl1_fundamental;
+
+public class array {
+    public static void main(String[] args) {
+        int ar[] = new int[]{1,3,5,7,9,2,4,6,8,10};
+
+        System.out.println(ar.length);
+    }
+}
