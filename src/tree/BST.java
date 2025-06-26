8889c02ea8d14c7e5ff922dd0d733e826c201c14
Animesh Singh <anianimesh905@gmail.com>
2025-06-26 15:58:05 +0530

    Update BST.java

--- src/tree/BST.java
+++ src/tree/BST.java
@@ -47,6 +47,41 @@ public class BST {
             return search(root.right, val);
     }
 
+    // Delete a node with value x from BST
+    public static Node delNode(Node root, int x) {
+        if (root == null) return root;
+
+        if (x < root.data) {
+            root.left = delNode(root.left, x);
+        } else if (x > root.data) {
+            root.right = delNode(root.right, x);
+        } else {
+            // Node to delete found
+
+            // Case 1: No left child
+            if (root.left == null)
+                return root.right;
+
+            // Case 2: No right child
+            if (root.right == null)
+                return root.left;
+
+            // Case 3: Two children
+            Node succ = getSuccessor(root);
+            root.data = succ.data;
+            root.right = delNode(root.right, succ.data);
+        }
+        return root;
+    }
+
+    // Get inorder successor (smallest value in right subtree)
+    public static Node getSuccessor(Node curr) {
+        curr = curr.right;
+        while (curr != null && curr.left != null)
+            curr = curr.left;
+        return curr;
+    }
+
 
     public static void main(String[] args) {
         int values[] = {5,1,2,4,3,7,6,9,8};
@@ -64,5 +99,7 @@ public class BST {
         System.out.println();
 
         System.out.println(search(root,7));
+        delNode(root, 5);
+        inorder(root);
     }
 }
