3ec9782ab2b70d3958e9f7ab02a58fb5473ee99d
Animesh Singh <anianimesh905@gmail.com>
2025-06-30 11:41:25 +0530

    Created SelectionSort.java

---
+++ src/Array/SelectionSort.java
@@ -0,0 +1,30 @@
+package src.Array;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {29, 10, 14, 37, 14};
+        selectionSort(arr);
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
