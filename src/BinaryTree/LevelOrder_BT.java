167b32dc436e9e22963160a8206409b382526162
Animesh Singh <anianimesh905@gmail.com>
2025-07-16 19:29:11 +0530

    Update

--- src/BinaryTree/LevelOrder_BT.java
+++ src/BinaryTree/LevelOrder_BT.java
@@ -1,14 +1,33 @@
 package src.BinaryTree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class LevelOrder_BT {
 
-    public ArrayList<ArrayList<Integer>> levelOrder(Node root){
+    public static ArrayList<ArrayList<Integer>> levelOrder(Node root){
         ArrayList<ArrayList<Integer>> e = new ArrayList<>();
 
         if(root == null)
-            
+            return e;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> a = new ArrayList<>();
+            for (int i = 0; i < size; i++){
+                Node node = q.poll();
+                a.add(node.data);
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            e.add(a);
+        }
+        return e;
     }
 
     public static void main(String[] args) {
@@ -19,5 +38,12 @@ public class LevelOrder_BT {
         root.left.right = new Node(5);
 
         ArrayList<ArrayList<Integer>> result = levelOrder(root);
+
+        System.out.println("Level Order Traversal of Tree:");
+        for (ArrayList<Integer> level : result) {
+            for (int num : level)
+                System.out.print(num + " ");
+            System.out.println();
+        }
     }
 }
